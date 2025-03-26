c3194eb7a3c5cec8a9d0fe8a982747db3f76891d
Florin Creta <cretaflorin4444@gmail.com>
2025-03-26 21:02:40 +0200

    Am creat clasa Movie in viewer.models

--- hollymovies/viewer/models.py
+++ hollymovies/viewer/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, TextField, IntegerField, DateField, ForeignKey, DO_NOTHING, DateTimeField, CASCADE
 
 # Create your models here.
 class Genre(Model):
@@ -6,3 +6,15 @@ class Genre(Model):
 
     def __str__(self):
         return f'Genre with id={self.id}, name={self.name}'
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    description = TextField()
+    rating = IntegerField()
+    released = DateField()
+    created = DateTimeField(auto_now_add=True)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+
+    def __str__(self):
+        return f'Movie with id={self.id}'
